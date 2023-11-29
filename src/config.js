7df761bff10ae0836490949df33fa66d5ee024f7
Sayantan Mishra <sayanmdn@gmail.com>
2023-11-29 22:18:29 +0530

    backend url update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:8080/";
-// export const URL =
-//   "https://fy5f66hx18.execute-api.ap-south-1.amazonaws.com/dev/";
+// export const URL = "http://localhost:8080/";
+export const URL =
+  "https://fy5f66hx18.execute-api.ap-south-1.amazonaws.com/dev/";
