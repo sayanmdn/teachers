476946cd7ac478a185cef75a45f05f3951b0106f
Sayantan Mishra <sayanmdn@gmail.com>
2023-12-04 01:05:32 +0530

    search update

--- src/components/Area1.js
+++ src/components/Area1.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner, ListGroup } from "react-bootstrap";
 import { useFormik } from "formik";
+import React, { useState } from "react";
+import axios from "axios";
+import { URL } from "../config";
 
 export function Area1() {
+  const [loading, setLoading] = useState(false);
+  const [testData, setTestData] = useState([]);
+
   const formik = useFormik({
     initialValues: {
       data: "",
     },
     onSubmit: (values) => {
-      console.log("form has been submitted with data: ", values.data);
+      setLoading(true);
+      setTestData();
+      axios
+        .post(`${URL}teachers/find`, { data: values.data })
+        .then((res) => {
+          if (res.data) {
+            setLoading(false);
+            setTestData(res.data);
+          }
+        })
+        .catch((error) => {
+          setLoading(false);
+          console.error(error);
+        });
     },
   });
 
@@ -50,6 +68,42 @@ export function Area1() {
             </Button>
           </Form>
         </div>
+      </div>
+      <br />
+      <div
+        className="data-api"
+        style={{ textAlign: "center", fontSize: "large" }}
+      >
+        {loading && (
+          <div>
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+            <p>(This typically requires around 2 seconds.)</p>
+          </div>
+        )}
+        {testData && (
+          <ListGroup
+            style={{
+              color: "black",
+              background: "blue",
+            }}
+          >
+            {testData.map((item, index) => (
+              <ListGroup.Item key={index}>
+                <strong>Name:</strong> {item.name}
+                <br />
+                <strong>Subjects:</strong> {item.subjects.join(", ")}
+                <br />
+                <strong>To Class:</strong> {item.toClass}
+                <br />
+                <strong>From Class:</strong> {item.fromClass}
+                <br />
+                <strong>Education:</strong> {item.education}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
       </div>
     </div>
   );
