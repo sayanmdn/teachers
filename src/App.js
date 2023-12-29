e3bc177ea8f64dc47709f9ce37872ff102ef68ea
Sayantan Mishra <sayanmdn@gmail.com>
2023-12-29 14:59:23 +0530

    unused routs blocked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import { useEffect } from "react";
 import { Navigationbar } from "./components/Navigationbar";
-import { Login } from "./components/Login";
 import { Area1 } from "./components/Area1";
 import { Register } from "./components/Register";
-import { Warehouse } from "./components/Warehouse";
 import { Profile } from "./components/Profile";
-import { NewsComponent } from "./components/NewsContainer";
-import { WriteComponent } from "./components/WriteComponent";
 import { StudentAccess } from "./components/StudentAccess";
 import reactGa from "react-ga";
+// import { Login } from "./components/Login";
+// import { Warehouse } from "./components/Warehouse";
+// import { NewsComponent } from "./components/NewsContainer";
+// import { WriteComponent } from "./components/WriteComponent";
 
 import { Provider } from "react-redux";
 
@@ -31,11 +31,11 @@ function App() {
             <Route exact path="/">
               <Area1 />
             </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
             <Route exact path="/register">
               <Register />
+            </Route>
+            {/* <Route exact path="/login">
+              <Login />
             </Route>
             <Route exact path="/warehouse">
               <Warehouse />
@@ -45,7 +45,7 @@ function App() {
             </Route>
             <Route exact path="/write">
               <WriteComponent />
-            </Route>
+            </Route> */}
             <Route exact path="/student-access">
               <StudentAccess />
             </Route>
