d80a8cf3ce152ab318aa85961a40cb8eb4c17f6e
Sayantan Mishra <sayanmdn@gmail.com>
2023-12-29 12:49:08 +0530

    con

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const URL = "http://localhost:8080/";
-// export const URL = "https://api.sayantanmishra.com/";
+// export const URL = "http://localhost:8080/";
+export const URL = "https://api.sayantanmishra.com/";
 // export const URL =
 //   "https://fy5f66hx18.execute-api.ap-south-1.amazonaws.com/dev/";
 //
